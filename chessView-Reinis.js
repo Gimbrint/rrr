c139186809d82ebff69009c523a930be79045008
Gimbrint <reinis.bartkevics@gmail.com>
2023-04-14 15:48:19 +0300

    idk2

--- chessView-Reinis.js
+++ chessView-Reinis.js
@@ -1,3 +1,24 @@
+class SimpleEvent {
+  constructor() {
+    this.eventHandlers = {};
+  }
+
+  addEventListener(eventType, eventHandler) {
+    // Create this type, if it doesn't allready exist!
+    if (!this.eventHandlers[eventHandler]) {
+      this.eventHandlers[eventType] = [];
+    }
+    
+    // We add a function (eventHandler) to the specified event type array
+    this.eventHandlers[eventType].push(eventHandler);
+  }
+    
+  dispatchEvents(eventType, eventInfo) {
+    // Call each function (eventHandler) of the specified event type and with the eventInfo parameter
+    this.eventHandlers[eventType].forEach(eh => eh(eventInfo));
+  }
+}
+
 class ChessBoardR extends SimpleEvent {
   constructor(container, size=60) {
     super();
@@ -6,22 +27,23 @@ class ChessBoardR extends SimpleEvent {
     this.container = container;
     this.size = size;
 
-    this.pieceImages = [
-      {       // Black
-        "K": "https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg",
-        "Q": "https://upload.wikimedia.org/wikipedia/commons/4/47/Chess_qdt45.svg",
-        "R": "https://upload.wikimedia.org/wikipedia/commons/f/ff/Chess_rdt45.svg",
-        "B": "https://upload.wikimedia.org/wikipedia/commons/9/98/Chess_bdt45.svg",
-        "N": "https://upload.wikimedia.org/wikipedia/commons/e/ef/Chess_ndt45.svg",
-        "P": "https://upload.wikimedia.org/wikipedia/commons/c/c7/Chess_pdt45.svg",
-      }, {      // White
-        "K": "https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg",
-        "Q": "https://upload.wikimedia.org/wikipedia/commons/1/15/Chess_qlt45.svg",
-        "R": "https://upload.wikimedia.org/wikipedia/commons/7/72/Chess_rlt45.svg",
-        "B": "https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg",
-        "N": "https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_nlt45.svg",
-        "P": "https://upload.wikimedia.org/wikipedia/commons/4/45/Chess_plt45.svg",
-      }];
+    this.pieceImages = [{ // White
+      "K": "https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg",
+      "Q": "https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg",
+      "R": "https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg",
+      "B": "https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg",
+      "K": "https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg",
+      "P": "https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg",
+      "B": "https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg"
+    }, {                  // Black
+      "K": "https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg",
+      "Q": "https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg",
+      "R": "https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg",
+      "B": "https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg",
+      "K": "https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg",
+      "P": "https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg",
+      "B": "https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg"
+    }];
 
     this.eventHandlers = {
       move: []
@@ -29,10 +51,9 @@ class ChessBoardR extends SimpleEvent {
   }
 
   drawBoard() {
-    const xmlns = "http://www.w3.org/2000/svg";
-
     for (let x = 0; x < 8; x++) {
       for (let y = 0; y < 8; y++) {
+        const xmlns = "http://www.w3.org/2000/svg";
         const square = document.createElementNS(xmlns, "rect");
         square.setAttribute("x", 20+x*this.size);
         square.setAttribute("y", 20+y*this.size);
@@ -52,24 +73,21 @@ class ChessBoardR extends SimpleEvent {
   }
 
   drawPieces(board) {
-    const xmlns = "http://www.w3.org/2000/svg";
-
-    for (let x = 0; x < board.length; x++) {
-      for (let y = 0; y < board.length; y++) {
-        const data = board[x][y];
-
-        if (data) {
-          const piece = document.createElementNS(xmlns, "image");
-          piece.setAttribute("href", this.pieceImages[data.player-1][data.type]);
-          piece.setAttribute("x", 20+x*this.size);
-          piece.setAttribute("y", 20+y*this.size);
-          piece.setAttribute("width", 60);
-          piece.setAttribute("height", 60);
-
-          this.container.appendChild(piece);
-        }
-      }
+    for (let i = 0; i < board.length; i++) {
+      
     }
+  }
+
+  drawPiece(x, y) {
+    const xmlns = "http://www.w3.org/2000/svg";
+    const piece = document.createElementNS(xmlns, "image");
+    piece.setAttribute("href", "https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg");
+    piece.setAttribute("x", 20 + x * this.size);
+    piece.setAttribute("y", 20 + y * this.size);
+    piece.setAttribute("width", 60);
+    piece.setAttribute("height", 60);
+
+    this.container.appendChild(piece);
   }
 
   move(txtMove) {
@@ -78,7 +96,7 @@ class ChessBoardR extends SimpleEvent {
 
   update(model) {
     this.drawBoard();
-    this.drawPieces(model.board);
+    this.drawPiece(2, 2);
     console.log("here");
   }
 }
